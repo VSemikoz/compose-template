a9ffcd916dcda8ed86a20b96fc7145a4932c1d51
VSemikoz <slava.semik@yandex.ru>
2024-04-03 20:32:18 +0300

    Fix SettingsUseCase rep

--- app/src/main/java/com/example/compose_template/domain/usecase/SettingsUseCase.kt
+++ app/src/main/java/com/example/compose_template/domain/usecase/SettingsUseCase.kt
@@ -1,15 +1,15 @@
 package com.example.compose_template.domain.usecase
 
-import com.example.compose_template.data.repository.SettingsRepository
 import com.example.compose_template.domain.entity.LanguageState
 import com.example.compose_template.domain.entity.SettingsEntity
 import com.example.compose_template.domain.entity.ThemeState
+import com.example.compose_template.domain.repository.ISettingsRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class SettingsUseCase @Inject constructor(
-    private val settingsRepository: SettingsRepository
+    private val settingsRepository: ISettingsRepository
 ) {
     fun updateTheme(themeState: ThemeState) = settingsRepository.updateTheme(themeState)
 
